5a7fe198ecdb7c271bde49f97e52ac291bd63584
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-04-19 03:14:17 -0500

    Added user validation using dummy credentials

--- forms.py
+++ forms.py
@@ -21,4 +21,4 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',
         validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Sign Up')
+    submit = SubmitField('Log In')
