a988079a4ea454b584ad415a7ad559d1b588a3b5
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-04-19 02:40:25 -0500

    email validation

--- flaskblog.py
+++ flaskblog.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__) # __name__ is the name of the module
+
+app.config['SECRET_KEY'] = '3af96cc5b5b78bd436fd117d788fc578' 
 
 # posts is a list of dictionary; there is one dictionarity for each blog post
 posts = [
-	{
-		'author': 'Sheel Dey',
-		'title': 'Blog Post 1',
-		'content': 'First post content',
-		'date_posted': 'April 14, 2019'
-	},
-	{
-		'author': 'Sheel Dey',
-		'title': 'Blog Post 2',
-		'content': 'Second post content',
-		'date_posted': 'April 15, 2019'
-	}
+    {
+        'author': 'Sheel Dey',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 14, 2019'
+    },
+    {
+        'author': 'Sheel Dey',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 15, 2019'
+    }
 ]
 
 # we use routes decorators to add pages to the web-
@@ -32,5 +35,18 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash('Account created for {}!'.format(form.username.data), 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
