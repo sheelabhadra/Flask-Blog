32c475aeb2da2980e7b237266afc07ee94dd4cb3
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-04-30 01:11:47 -0500

    Added login check

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt # for user authentication
+from flask_login import LoginManager # to manage login of users (Login page)
 
 app = Flask(__name__) # __name__ is the name of the module
 
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = '3af96cc5b5b78bd436fd117d788fc578'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
