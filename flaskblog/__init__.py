2eaa6f707e1f3603fccd39683f4aa0d25358200f
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-04-30 00:49:55 -0500

    Added password encryption

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt # for user authentication
 
 app = Flask(__name__) # __name__ is the name of the module
 
 app.config['SECRET_KEY'] = '3af96cc5b5b78bd436fd117d788fc578'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
