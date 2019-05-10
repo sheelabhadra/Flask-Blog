57e9cac05b99f2a980f534803fbb51bb56661ed7
Sheelabhadra Dey <sheelabhadra@gmail.com>
2019-05-09 19:41:54 -0500

    Added feature to update a post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, PostForm
 from flaskblog.models import User, Post
@@ -70,7 +70,8 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html', title='New Post', 
+        form=form, legend='New Post')
 
 # Go to a specific page for a single post - ID of a post is a part of the route
 @app.route("/post/<int:post_id>")
@@ -79,7 +80,24 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
-
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', 
+        form=form, legend='Update Post')
 
 
 
